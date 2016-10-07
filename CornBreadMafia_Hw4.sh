35271e9c67d0376d5598f36e67789d123f4f7270
Chia Che Chang <chiachechang@mail.weber.edu>
2016-10-07 16:28:02 -0600

    Adding the shell script

---
+++ CornBreadMafia_Hw4.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: CornBreadMafia_Hw4.sh
+# 
+#         USAGE: ./CornBreadMafia_Hw4.sh 
+# 
+#   DESCRIPTION: Shell script for Assignment 4
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Chia Che Chang (), chiachechang@mail.weber.edu
+#  ORGANIZATION: 
+#       CREATED: 10/07/2016 16:26
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+
