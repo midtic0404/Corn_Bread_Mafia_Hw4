e495665618d53c7a3374db252cd7fe7ae86410f2
Chia Che Chang <chiachechang@mail.weber.edu>
2016-10-10 15:18:01 -0600

    Adding getopts part to .sh file

--- CornBreadMafia_Hw4.sh
+++ CornBreadMafia_Hw4.sh
@@ -19,4 +19,43 @@
 
 #set -o nounset                              # Treat unset variables as an error
 
+#1 step 1 setting up getops
 
+usage(){
+	echo "Usage ./CornBreadMafia_Hw4.sh [-y YEAR] [-e email] [-u user] [-p passwd]"
+	echo "YEAR and email options are required"
+}
+
+if [[ ${#} -lt 4 ]] || [[ $1 == "--help" ]]
+then
+	usage
+	exit 1
+fi
+
+
+
+while getopts ":y:e:u:p:" opt;
+do
+	case $opt in
+		y)
+			YEAR=$OPTARG
+			;;
+		e)
+			email=$OPTARG
+			;;
+		u)
+			user=$OPTARG
+			;;
+		p)
+			passwd=$OPTARG
+			;;
+		*)
+			usage
+			exit 2
+			;;
+	esac
+done
+
+
+
+exit 0
